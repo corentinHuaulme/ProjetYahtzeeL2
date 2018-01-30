ff33e7daa66982c912eb043682cdc7724d6983c2
Lovelace973 <andydinga@hotmail.fr>
2018-01-30 15:11:45 +0100

    1er version du menu
    1er cours

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	
+		printf("\n\n\n\n\n\n\n\n\n");
+		printf("	   #   #   ###     #   #   #####  #####  #####  #####  \n");
+		printf("	   #   #  #   #    #   #     #        #  #      #      \n");
+		printf("	     #    #   #    #   #     #       #   #      #      \n");
+		printf("	     #    #####    #####     #      #    ####   ####   \n");
+		printf("	     #    #   #    #   #     #     #     #      #      \n");
+		printf("	     #    #   #    #   #     #    #      #      #      \n");
+		printf("	     #    #   #    #   #     #   ######  #####  #####  \n");
+		printf("\n		+---------------------------------------+\n");
+		printf("		|					|");
+		printf("\n		|		Menu :			|\n");
+		printf("		|					|\n");
+		printf("		|	1 - Lancer une partie		|\n");
+		printf("		|	2 - Regles du jeux     		|\n");
+		printf("		|	3 - Quitter           		|\n");
+		printf("		|					|");
+        printf("\n		+---------------------------------------+\n");
+
+
+}
+	
