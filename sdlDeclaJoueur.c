fc51ac3dc7cee7dd59edbd8fade79f81b846d2bc
Corentin HUAULME <huaulmecorentin@gmail.com>
2018-03-19 16:24:53 +0100

    ajout fenetre Joueur

--- sdlDeclaJoueur.c
+++ sdlDeclaJoueur.c
@@ -1,96 +1,206 @@
 #include <stdio.h>
-#include "sdlJeu.h"
+#include "sdlDeclaJoueur.h"
+#include "sdl.h"
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_ttf.h"
 #include "SDL2/SDL_image.h"
 
+SDL_Rect * rLettre1 = NULL;
+SDL_Rect * rLettre2 = NULL;
+SDL_Rect * rLettre3 = NULL;
+SDL_Rect * rLettreUp1 = NULL;
+SDL_Rect * rLettreDown1 = NULL;
+SDL_Rect * rLettreUp2 = NULL;
+SDL_Rect * rLettreDown2 = NULL;
+SDL_Rect * rLettreUp3 = NULL;
+SDL_Rect * rLettreDown3 = NULL;
+
+SDL_Surface* sLettre1 = NULL;
+SDL_Surface* sLettre2 = NULL;
+SDL_Surface* sLettre3 = NULL;
+
+char ltr1='A';
+char ltr2='A';
+char ltr3='A';
+
 int afficheFenetreJoueur(SDL_Window* win, SDL_Renderer* ren)
 {
-	//Le pointeur vers les surfaces incluse dans la fenetre
-	SDL_Surface* sTitre = NULL;
-	SDL_Surface* sJoueur = NULL;
-	SDL_Surface* sRetour = NULL;
-
-    // Set render color to red ( background will be rendered in this color )
-	SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
-
-    // Clear winow
-	SDL_RenderClear( renderer );
-
-	TTF_Font* Sans = TTF_OpenFont("fonts/Montserrat-Regular.ttf", 24); //this opens a font style and sets a size
-
-	if(Sans == NULL){
-		printf("ERROR");
-	}
-
+	SDL_RenderClear(ren);
+	SDL_RenderPresent(ren);
+	TTF_Font* Sans = TTF_OpenFont("fonts/Montserrat-Regular.ttf", 50);
 	SDL_Color White = {255, 255, 255};
 
-	sTitre = TTF_RenderText_Solid(Sans, "Yahtzee du pauvre", White);
+	SDL_Surface* sSuivant = NULL;
+	SDL_Surface* sLettreUp1 = NULL;
+	SDL_Surface* sLettreDown1 = NULL;
+	SDL_Surface* sLettreUp2 = NULL;
+	SDL_Surface* sLettreDown2 = NULL;
+	SDL_Surface* sLettreUp3 = NULL;
+	SDL_Surface* sLettreDown3 = NULL;
 
-	SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, sTitre);
+	sSuivant = TTF_RenderText_Solid(Sans, "Suivant", White);
+	SDL_Texture* mSuivant = SDL_CreateTextureFromSurface(ren, sSuivant);
 	
-	SDL_Rect Message_rect;
-	Message_rect.x = 0; 
-	Message_rect.y = 0;
-	Message_rect.w = 1000;
-	Message_rect.h = 100;
-
-	SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
-
-	//SDL_RenderPresent( renderer );
-
-    
-	sJouer = TTF_RenderText_Solid(Sans, "Jouer", White);
-
-	SDL_Texture* mJouer = SDL_CreateTextureFromSurface(renderer, sJouer);
+	SDL_Rect* rSuivant = NULL;
+	rSuivant = malloc(sizeof(SDL_Rect));
+	rSuivant->x = 400; 
+	rSuivant->y = 450;
+	SDL_QueryTexture(mSuivant, NULL, NULL, &(rSuivant->w), &(rSuivant->h));
+	SDL_RenderCopy(ren, mSuivant, NULL, rSuivant);
 	
-	SDL_Rect rJouer;
-	rJouer.x = 350; 
-	rJouer.y = 200;
-	rJouer.w = 300;
-	rJouer.h = 100;
+	char * l = &(ltr1);
+	l = &(l[0]);
 
-	SDL_RenderCopy(renderer, mJouer, NULL, &rJouer);
+	sLettre1 = TTF_RenderText_Solid(Sans,&(l[0]), White);
+	SDL_Texture* mLettre1 = SDL_CreateTextureFromSurface(ren, sLettre1);
+	
+	rLettre1 = malloc(sizeof(SDL_Rect));
+	rLettre1->x = 400; 
+	rLettre1->y = 250;
+	SDL_QueryTexture(mLettre1, NULL, NULL, &(rLettre1->w), &(rLettre1->h));
+	SDL_RenderCopy(ren, mLettre1, NULL, rLettre1);
+	
 
-	SDL_RenderPresent( renderer );	
+	sLettre2 = TTF_RenderText_Solid(Sans, &(ltr2), White);
+	SDL_Texture* mLettre2 = SDL_CreateTextureFromSurface(ren, sLettre2);
+	
+	rLettre2 = malloc(sizeof(SDL_Rect));
+	rLettre2->x = 450; 
+	rLettre2->y = 250;
+	SDL_QueryTexture(mLettre2, NULL, NULL, &(rLettre2->w), &(rLettre2->h));
+	SDL_RenderCopy(ren, mLettre2, NULL, rLettre2);
 
 
-	if( pWindow )
-	{
-        	int running = 1; 
-		while(running) { 
-			SDL_Event e; 
-			while(SDL_PollEvent(&e)) { 
-				switch(e.type) { 
-					case SDL_QUIT: running = 0; 
-					break; 
-					case SDL_MOUSEBUTTONDOWN:
-					    switch (e.button.button)
-					    {
-						case SDL_BUTTON_LEFT:
-							if(check_click_in_rect(e.motion.x, e.motion.y, &rJouer)){
-								fenetreJeu(pWindow, renderer);
+	sLettre3 = TTF_RenderText_Solid(Sans, &(ltr3), White);
+	SDL_Texture* mLettre3 = SDL_CreateTextureFromSurface(ren, sLettre3);
+	
+	rLettre3 = malloc(sizeof(SDL_Rect));
+	rLettre3->x = 500; 
+	rLettre3->y = 250;
+	SDL_QueryTexture(mLettre3, NULL, NULL, &(rLettre3->w), &(rLettre3->h));
+	SDL_RenderCopy(ren, mLettre3, NULL, rLettre3);
+
+	sLettreUp1 = IMG_Load("flecheHaut.png");
+
+	SDL_Texture* mLettreUp1 = SDL_CreateTextureFromSurface(ren, sLettreUp1);
+	
+	rLettreUp1 = malloc(sizeof(SDL_Rect));
+	rLettreUp1->x = 393; 
+	rLettreUp1->y = 200;
+	rLettreUp1->w = 50;
+	rLettreUp1->h = 50;
+
+	SDL_RenderCopy(ren, mLettreUp1, NULL, rLettreUp1);
+
+
+	sLettreUp2 = IMG_Load("flecheHaut.png");
+
+	SDL_Texture* mLettreUp2 = SDL_CreateTextureFromSurface(ren, sLettreUp2);
+	
+	rLettreUp2 = malloc(sizeof(SDL_Rect));
+	rLettreUp2->x = 443; 
+	rLettreUp2->y = 200;
+	rLettreUp2->w = 50;
+	rLettreUp2->h = 50;
+
+	SDL_RenderCopy(ren, mLettreUp2, NULL, rLettreUp2);
+
+
+	sLettreUp3 = IMG_Load("flecheHaut.png");
+
+	SDL_Texture* mLettreUp3 = SDL_CreateTextureFromSurface(ren, sLettreUp3);
+	
+	rLettreUp3 = malloc(sizeof(SDL_Rect));
+	rLettreUp3->x = 493; 
+	rLettreUp3->y = 200;
+	rLettreUp3->w = 50;
+	rLettreUp3->h = 50;
+
+	SDL_RenderCopy(ren, mLettreUp3, NULL, rLettreUp3);
+
+
+	sLettreDown1 = IMG_Load("flecheBas.png");
+
+	SDL_Texture* mLettreDown1 = SDL_CreateTextureFromSurface(ren, sLettreDown1);	
+	rLettreDown1 = malloc(sizeof(SDL_Rect));
+	rLettreDown1->x = 393; 
+	rLettreDown1->y = 315;
+	rLettreDown1->w = 50;
+	rLettreDown1->h = 50;
+
+	SDL_RenderCopy(ren, mLettreDown1, NULL, rLettreDown1);
+
+
+	sLettreDown2 = IMG_Load("flecheBas.png");
+
+	SDL_Texture* mLettreDown2 = SDL_CreateTextureFromSurface(ren, sLettreDown2);
+	
+	rLettreDown2 = malloc(sizeof(SDL_Rect));
+	rLettreDown2->x = 443; 
+	rLettreDown2->y = 315;
+	rLettreDown2->w = 50;
+	rLettreDown2->h = 50;
+
+	SDL_RenderCopy(ren, mLettreDown2, NULL, rLettreDown2);
+
+
+	sLettreDown3 = IMG_Load("flecheBas.png");
+
+	SDL_Texture* mLettreDown3 = SDL_CreateTextureFromSurface(ren, sLettreDown3);
+	
+	rLettreDown3 = malloc(sizeof(SDL_Rect));
+	rLettreDown3->x = 493; 
+	rLettreDown3->y = 315;
+	rLettreDown3->w = 50;
+	rLettreDown3->h = 50;
+
+	SDL_RenderCopy(ren, mLettreDown3, NULL, rLettreDown3);
+
+	SDL_RenderPresent(ren);
+}
+
+
+int fenetreJoueur(SDL_Window* win, SDL_Renderer* ren)
+{
+	afficheFenetreJoueur(win,ren);
+	if(win){
+		int running = 1;
+		while(running){
+			while(running) {
+				SDL_Event e; 
+				while(SDL_PollEvent(&e)) { 
+					switch(e.type) { 
+						case SDL_QUIT: running = 0; break;
+						case SDL_MOUSEBUTTONDOWN:
+							switch (e.button.button)
+							{
+								case SDL_BUTTON_LEFT:
+									if(check_click_in_rect(e.motion.x, e.motion.y, rLettreUp1)){
+											ltr1 = ltr1-1;
+											afficheFenetreJoueur(win,ren);											
+									}if(check_click_in_rect(e.motion.x, e.motion.y, rLettreDown1)){
+											ltr1=ltr1+1;
+											afficheFenetreJoueur(win,ren);											
+									}if(check_click_in_rect(e.motion.x, e.motion.y, rLettreUp2)){
+											ltr2=ltr2-1;
+											//afficheFenetreJoueur(win,ren);											
+									}if(check_click_in_rect(e.motion.x, e.motion.y, rLettreDown2)){
+											ltr2=ltr2+1;
+											//afficheFenetreJoueur(win,ren);											
+									}if(check_click_in_rect(e.motion.x, e.motion.y, rLettreUp3)){
+											ltr3=ltr3-1;
+											//afficheFenetreJoueur(win,ren);											
+									}if(check_click_in_rect(e.motion.x, e.motion.y, rLettreDown3)){
+											ltr3=ltr3+1;
+											//afficheFenetreJoueur(win,ren);											
+									}
+									break;
 							}
-						    break;
-						case SDL_BUTTON_RIGHT:
-						    SDL_ShowSimpleMessageBox(0, "Mouse", "Right button was pressed!", pWindow);
-						    break;
-						default:
-						    SDL_ShowSimpleMessageBox(0, "Mouse", "Some other button was pressed!", pWindow);
-						    break;
-					    }
-					    break;
-				} 
-			} 
+							break;
+					}
+
+				}
+			}
 		}
-	} else {
-		fprintf(stderr,"Erreur de création de la fenêtre: %s\n",SDL_GetError());
 	}
-
-	//Destruction de la fenetre
-	SDL_DestroyWindow(pWindow);
-	TTF_Quit();
-    SDL_Quit();
-
-    return 0;
 }
---
+++ sdlDeclaJoueur.h
@@ -0,0 +1,11 @@
+#ifndef _PROJET_SDLJEU_H
+#define _PROJET_SDLJEU_H
+#include <stdio.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL_image.h"
+
+int fenetreJoueur(SDL_Window* win, SDL_Renderer* ren);
+int afficheFenetreJoueur(SDL_Window* win, SDL_Renderer* ren);
+
+#endif
