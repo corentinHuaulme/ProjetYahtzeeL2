251f71f32b1e01edd9ba1a765c2cb3e7b21d2104
Lovelace973 <andydinga@hotmail.fr>
2018-03-19 16:24:33 +0100

    menu sdl v1

---
+++ sdlCredit.h
@@ -0,0 +1,10 @@
+#ifndef _PROJET_SDLCREDIT_H
+#define _PROJET_SDLCREDIT_H
+#include <stdio.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL_image.h"
+
+void afficheCredit(SDL_Window* win, SDL_Renderer* ren);
+
+#endif
---
+++ sdlRegle.h
@@ -0,0 +1,10 @@
+#ifndef _PROJET_SDLREGLE_H
+#define _PROJET_SDLREGLE_H
+#include <stdio.h>
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_ttf.h"
+#include "SDL2/SDL_image.h"
+
+void afficheRegle(SDL_Window* win, SDL_Renderer* ren);
+
+#endif
